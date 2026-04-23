ba94b6bab679416ccc23db47c4acee5976940da8
RealRottale1 <camarkham2@gmail.com>
2026-04-23 08:49:46 -0400

    _

--- solver.java
+++ solver.java
@@ -1,4 +1,9 @@
-
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.*;
 
 class Solver {
     public record Cord(int y, int x) {}
@@ -7,6 +12,10 @@ class Solver {
         for (int y = 0; y < rows < y++) {
             for (int x = 0; x < cols; x++) {
                 Cord masterCord = cord(y, x);
+                HashSet<cord> usedCells = new HashSet<>(masterCord);
+                ArrayList<cord> nextNodes = getNeighbors(masterCord, usedCells);
+                ArrayList<ArrayList<cord>> masterBranch = new ArrayList<>();
+                masterBranch.add(nextNodes);
             }
         }
     }
